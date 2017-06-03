0188e7b54b46b69c9f7dd3a62a122faec737d4aa
Piggat <nguyenxuankien@alimama.vn>
2017-06-03 15:01:45 +0700

    write practices for week 3

---
+++ 003/XOChess.ts
@@ -0,0 +1,33 @@
+/**
+ * Created by Piggat on 6/3/2017.
+ */
+export class XOChess {
+    /**
+     *
+     * @param board
+     * @returns {number}
+     */
+    public static getPlayerWon(board:number[][]):number {
+        return 0;
+    }
+
+    /**
+     *
+     * @param board
+     * @param player
+     * @returns {boolean}
+     */
+    public static isPlayerWinWithoutBlocked(board: number[][], player:number):boolean {
+        return false;
+    }
+
+    /**
+     *
+     * @param board
+     * @param player
+     * @returns {number}
+     */
+    public static countThreeStones(board: number[][], player: number): number {
+        return 0;
+    }
+}
